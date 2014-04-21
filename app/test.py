f3568d5b309aa299cf6114e4864daac916583248
vemacs <gentoo@outlook.com>
2014-04-21 12:45:31 -0400

    A sinks

--- app/test.py
+++ app/test.py
@@ -1,4 +1,4 @@
-import mcstatus, yaml, time
+import mcstatus, yaml, time, threading
 
 data = {}
 
@@ -13,14 +13,16 @@ for category in servers_config:
         ip = servers_config[category][server]
         status = mcstatus.McServer(ip.split("/")[0], int(ip.split("/")[1]))
         data[category][server] = status
-        status.Update()
 
+def update_all():
+    for category in data:
+        for server in data[category]:
+            status = data[category][server]
+            threading.Thread(target=lambda: status.Update()).start()
+
+update_all()
 time.sleep(2)
-
 for category in data:
-    print category
     for server in data[category]:
-        print server
         status = data[category][server]
-        print "available: " + str(status.available)
-        print str(status.num_players_online) + "/" + str(status.max_players_online)
+        print category + ":" + server + ":" + str(status.num_players_online) + "/" + str(status.max_players_online)
